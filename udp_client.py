ca4dcf5db02f04fdabd5b2647d75ac65ad82848e
Rafael Cruz <rafaelcruz@outlook.com>
2019-12-24 21:22:18 -0300

    add

--- udp_client.py
+++ udp_client.py
@@ -7,30 +7,31 @@ from util import *
 import subprocess
 
 logger = logging.getLogger()
-core_file = '/home/$USER/.var/app/org.libretro.RetroArch/config/retroarch/cores/snes9x_libretro.so'
-rom_file = '~/Downloads/sf2.zip'
+core_file = '/home/$USER/.var/app/org.libretro.RetroArch/config/retroarch/cores/blastem_libretro.so'
+rom_file = '~/Downloads/sr2.zip'
 cmd_host = 'org.libretro.RetroArch --host --port={} -L {} {}'
 cmd_client = 'org.libretro.RetroArch --connect={} --port={} -L {} {}'
 
 def listen(sock, player):
+    started = False
     print ("Starting listening...")
     while True:
         data, addr = sock.recvfrom(1024)
         print('RECEIVED: {}'.format(addr))
         # player_addr = msg_to_addr_player(data)
 
-        if player == '2':
+        if started is False and player == '2':
             logger.info("Starting as host... port: %s", sock.getsockname()[1])
             cmd = cmd_host.format(sock.getsockname()[1], core_file, rom_file)
             logger.info("Running: {}".format(cmd))
             p = subprocess.Popen(cmd, shell=True)
-            break
-        else:
+            started = True
+        elif started is False:
             logger.info("Connecting at host... %s:%s", addr[0], addr[1])
             cmd = cmd_client.format(addr[0], addr[1], core_file, rom_file)
             logger.info("Running: {}".format(cmd))
             p = subprocess.Popen(cmd, shell=True)
-            break
+            started = True
 
 def main(host='3.15.42.116', port=5005):
     print('connecting: {}:{}'.format(host, port))
@@ -44,7 +45,8 @@ def main(host='3.15.42.116', port=5005):
         addr = msg_to_addr_player(data)
         attempts = 1
         threading.Thread(target = listen, args = (sock,addr[2],)).start()
-        while attempts < 10:
+        # while attempts < 10:
+        while True:
             print('{} - Sending: {}'.format(attempts, (addr[0],addr[1])))
             sock.sendto(b'0', (addr[0],addr[1]))
             time.sleep(1)
